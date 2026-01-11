3fd5f05be54119982aee9db0b850269a0776a7e1
Prajwal2246 <janbandhuprajwal28@gmail.com>
2026-01-11 16:29:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const body = document.querySelector("body");
+const p = document.createElement("p");
+p.textContent = new Date().toLocaleString();
+body.appendChild(p);
